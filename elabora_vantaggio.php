613a5a1ea79cc6ce5b9c77daa271906bfed3d390
EmquCity <115823093+EmquCity@users.noreply.github.com>
2024-03-11 22:51:56 +0100

    Update elabora_vantaggio.php

--- elabora_vantaggio.php
+++ elabora_vantaggio.php
@@ -5,31 +5,33 @@
     $puntiPareggio = 1;
     $puntiSconfitta = 0;
 
-    //if (isset($_GET['vittoria'], $_GET['giocatore1_id'], $_GET['giocatore2_id'], $_GET['vantaggioPezzi1'], $_GET['vantaggioPezzi2'])) {
-
-    if (isset($_GET['vittoria'], $_GET['giocatore1_id'], $_GET['giocatore2_id'])) {
+    if(isset($_GET['vittoria'], $_GET['giocatore1_id'], $_GET['giocatore2_id'], $_GET['vantaggioPz1'])) {
         $vittoria = $_GET['vittoria'];
         $giocatore1_id = $_GET['giocatore1_id'];
         $giocatore2_id = $_GET['giocatore2_id'];
-        //$vantaggioPz1 = $_GET['vantaggioPezzi1'];
-        //$vantaggioPz2 = $_GET['vantaggioPezzi2'];
+        $vantaggioPz1 = $_GET['vantaggioPz1'];
+        $vantaggioPz2 = $_GET['vantaggioPz2'];
 
         if ($vittoria == 1) {
-            $query = "UPDATE giocatore SET parVinte = parVinte + 1, punti = punti + $puntiVittoria /*vantPezzi = vantPezzi + $vantaggioPz1*/ WHERE idGiocatore = $giocatore1_id;";
+            $query = "UPDATE giocatore SET parVinte = parVinte + 1, punti = punti + $puntiVittoria, vantPezzi = vantPezzi + $vantaggioPz1 WHERE idGiocatore = $giocatore1_id;";
             mysqli_query($conn, $query);
-            $query = "UPDATE giocatore SET parPerse = parPerse + 1 /*vantPezzi = vantPezzi + $vantaggioPz2*/ WHERE idGiocatore = $giocatore2_id";
+            $query = "UPDATE giocatore SET parPerse = parPerse + 1, vantPezzi = vantPezzi + $vantaggioPz2 WHERE idGiocatore = $giocatore2_id";
             mysqli_query($conn, $query);
             header("location: partite.php");
         } elseif ($vittoria == 'X') {
-            $query = "UPDATE giocatore SET parPatte =  parPatte + 1, punti = punti + $puntiPareggio /*vantPezzi = vantPezzi + $vantaggioPz1, vantPezzi = vantPezzi + $vantaggioPz2*/ WHERE idGiocatore IN ($giocatore1_id, $giocatore2_id)";
+            $query = "UPDATE giocatore SET parPatte =  parPatte + 1, punti = punti + $puntiPareggio, vantPezzi = vantPezzi + $vantaggioPz1 WHERE idGiocatore = $giocatore1_id;";
+            mysqli_query($conn, $query);
+            $query = "UPDATE giocatore SET parPatte = parPatte + 1, punti = punti + $puntiPareggio, vantPezzi = vantPezzi + $vantaggioPz2 WHERE idGiocatore = $giocatore2_id;";
             mysqli_query($conn, $query);
             header("location: partite.php");
         } elseif($vittoria == 2) {
-            $query = "UPDATE giocatore SET parVinte = parVinte + 1, punti = punti + $puntiVittoria /*vantPezzi = vantPezzi + $vantaggioPz1*/ WHERE idGiocatore = $giocatore2_id";
+            $query = "UPDATE giocatore SET parVinte = parVinte + 1, punti = punti + $puntiVittoria, vantPezzi = vantPezzi + $vantaggioPz1 WHERE idGiocatore = $giocatore2_id";
             mysqli_query($conn, $query);
-            $query = "UPDATE giocatore SET parPerse = parPerse + 1 /*vantPezzi = vantPezzi + $vantaggioPz2*/ WHERE idGiocatore = $giocatore1_id";
+            $query = "UPDATE giocatore SET parPerse = parPerse + 1, vantPezzi = vantPezzi + $vantaggioPz2 WHERE idGiocatore = $giocatore1_id";
             mysqli_query($conn, $query);
             header("location: partite.php");
         }
+    }else{
+        echo"frocio";
     }
 ?>
